10258e4b4e04ce901386ea43b0e2586a2e6d9706
Quezler <Quezler@me.com>
2017-10-26 10:13:37 +0200

    Summon and hook event listener

---
+++ src/TerrazineComposerEventsServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Terrazine\ComposerEvents;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TerrazineLaravelIdeHelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            Event::listen(PostAutoloadDump::class, function () {
+                return function (InputInterface $input, OutputInterface $output){
+                    Artisan::call('ide-helper:models', [
+                        '--write' => true,
+                        '--reset' => true,
+                    ], $output);
+                    Artisan::call('ide-helper:generate', [], $output);
+                    Artisan::call('ide-helper:meta', [], $output);
+                };
+            });
+        }
+    }
+}
